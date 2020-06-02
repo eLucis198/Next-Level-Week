9b7e9981318ce1a65874c5517e6ae8c81b0ea470
Guilherme Guedes <30610742+eLucis198@users.noreply.github.com>
2020-06-02 19:12:02 -0300

    Add
    Added TypeScript, ts-node-dev, ESLint, Express and simple src/ files.

---
+++ server/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+
+import routes from './routes'
+
+class App {
+  public express: express.Application
+
+  constructor () {
+    this.express = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  private middlewares () {
+    this.express.use(express.json())
+  }
+
+  private routes () {
+    this.express.use(routes)
+  }
+}
+
+export default new App().express
