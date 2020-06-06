5981801b09373a0c3cb618c1ad7e62eb1bf1f415
Guilherme Guedes <30610742+eLucis198@users.noreply.github.com>
2020-06-06 04:43:01 -0300

    Add Points Screen
    Added the screen that has the points in a map

--- server/src/controller/ItemController.ts
+++ server/src/controller/ItemController.ts
@@ -8,7 +8,7 @@ class ItemController {
       return {
         id: item.id,
         title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`
+        image_url: `http://192.168.0.11:3333/uploads/${item.image}`
       }
     })
 
